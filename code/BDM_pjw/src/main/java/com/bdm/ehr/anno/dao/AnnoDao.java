1e6b26a5feefd3964db812f56dd2b9112c13e30e
DESKTOP-NC4BC5G\82108 <jinwooace@naver.com>
2024-01-27 16:05:27 +0900

    2401270405

---
+++ code/BDM_pjw/src/main/java/com/bdm/ehr/anno/dao/AnnoDao.java
@@ -0,0 +1,9 @@
+package com.bdm.ehr.anno.dao;
+
+import com.bdm.ehr.anno.domain.AnnoVO;
+import com.bdm.ehr.cmn.WorkDiv;
+
+public interface AnnoDao extends WorkDiv<AnnoVO> { 
+
+}
+ 
---
+++ code/BDM_pjw/src/main/java/com/bdm/ehr/anno/dao/AnnoDaoImpl.java
@@ -0,0 +1,60 @@
+package com.bdm.ehr.anno.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.bdm.ehr.anno.domain.AnnoVO;
+
+@Repository
+public class AnnoDaoImpl implements AnnoDao {
+
+	final String NAMESPACE = "com.bdm.ehr.anno";
+	
+	@Autowired
+	SqlSessionTemplate sqlSessionTemplate;
+	
+	@Override
+	public int doUpdate(AnnoVO inVO) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int doDelete(AnnoVO inVO) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public AnnoVO doSelectOne(AnnoVO inVO) throws SQLException {
+		AnnoVO outVO=new AnnoVO();
+		System.out.println("========================");
+		System.out.println("=AnnoDaoImpl=");
+		System.out.println("=inVO="+inVO);
+		System.out.println("========================");
+		
+		//namespace+id -> com.pcwk.ehr.anno.doSelectOne
+		String statement = NAMESPACE + ".doSelectOne";
+		outVO = sqlSessionTemplate.selectOne(statement, inVO);
+		
+		System.out.println("=outVO="+outVO);
+		return outVO;
+	}
+
+	@Override
+	public int doSave(AnnoVO inVO) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<AnnoVO> doRetrieve(AnnoVO inVO) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
