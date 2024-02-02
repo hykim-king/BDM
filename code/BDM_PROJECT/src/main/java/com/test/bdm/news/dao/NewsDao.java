bb199a9fff90d6b46d048ac04801a0ee1713c52c
양갱 <148629426+Olaf247@users.noreply.github.com>
2024-02-02 16:46:06 +0900

    news

---
+++ code/BDM_PROJECT/src/main/java/com/test/bdm/news/dao/NewsDao.java
@@ -0,0 +1,35 @@
+package com.test.bdm.news.dao;
+
+import java.sql.SQLException;
+
+
+import com.test.bdm.cmn.WorkDiv;
+import com.test.bdm.news.domain.NewsVO;
+
+public interface NewsDao extends WorkDiv<NewsVO> {
+
+	
+	/**
+	 * board_seq 
+	 * @return int
+	 * @throws SQLException
+	 */
+	int getNewsSeq()throws SQLException;
+	/**
+	 * 글제목으로 삭제: test only
+	 * @param inVO
+	 * @return int
+	 * @throws SQLException
+	 */
+	int doDeleteAll(NewsVO inVO)throws SQLException;
+	
+	/**
+	 * 글제목으로 삭제: test only
+	 * @param inVO
+	 * @return int
+	 * @throws SQLException
+	 */
+	
+	int updateReadCnt(NewsVO inVO)throws SQLException;  
+	
+}
