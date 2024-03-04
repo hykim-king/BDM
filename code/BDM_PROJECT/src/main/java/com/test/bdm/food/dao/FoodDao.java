123078a75bfe8c95895ba4460826c9021d501fa9
heosungmu <sungmu1193@gmail.com>
2024-03-04 21:12:51 +0900

    2024-03-04

--- code/BDM_PROJECT/src/main/java/com/test/bdm/food/dao/FoodDao.java
+++
@@ -1,8 +0,0 @@
-package com.test.bdm.food.dao;
-
-import java.util.List;
-
-public interface FoodDao {
-
-	int doSaveFood(String userId, List<String> selectedFoodCode, List<Double> amountList, String divs);
-}
